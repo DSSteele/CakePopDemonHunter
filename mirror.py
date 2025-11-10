e9a5f8f9afddb7dca676c6a92e62989420e59890
David Steele <dsteele2@mail.niagara.edu>
2025-11-10 10:53:19 -0500

    Added text feedback to user in top-left corner

--- mirror.py
+++ mirror.py
@@ -1,6 +1,6 @@
-# This file will be a minimal OpenCV file for miroring the webcam
+# This file will be a minimal OpenCV file for mirroring the webcam
 
-print('This is miror.py')
+print('This is mirror.py')
 
 import numpy as np
 import cv2 as cv
@@ -13,9 +13,9 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Using cv.flip() method
-    # Use Flip code 1 to flip horizontally
+    # Flip every frame in the horizontal direction
     frame = cv.flip(frame, 1)
+    height, width = frame.shape[:2]
 
     # if frame is read correctly ret is True
     if not ret:
@@ -23,8 +23,31 @@ while True:
         break
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Calculate change in gray
+    delta = cv.absdiff(gray, prev_gray)
+
+    # Display text
+    cv.putText(frame, str(delta), (50,300),
+        cv.FONT_HERSHEY_SIMPLEX,
+        2,              # fontScale
+        (255,255,255),  # color
+        4)              # thickness
+
+    cv.putText(frame, "Hello", (50,100),
+        cv.FONT_HERSHEY_SIMPLEX,
+        2,              # fontScale
+        (132,38,80),    # color
+        4)              # thickness
+
     # Display the resulting frame
-    cv.imshow('frame', gray)
+    #cv.imshow('frame', gray)
+    cv.imshow('frame', frame)
+
+    #Save previous frame
+    prev_gray = gray
+
+    # See if user wants to quit
     if cv.waitKey(1) == ord('q'):
         break
 
