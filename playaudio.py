aba1001c04fd0bfe074ed405b83f40d304bd4044
David Steele <dsteele2@mail.niagara.edu>
2025-11-28 13:50:10 -0500

    Changed audio file path from absolute to relative

--- playaudio.py
+++ playaudio.py
@@ -1,7 +1,14 @@
 # This file will play a local audio file
-
 print('This is playaudio.py')
 
+import os
 from playsound import playsound
 
-playsound("/Users/cislab/DSS/golden_parody.mp3")
+# get directory where this script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# build path to audio file relative to script
+audio_file = os.path.join(script_dir, "golden_parody.mp3").replace("\\", "/")
+
+# play audio
+playsound(audio_file)
