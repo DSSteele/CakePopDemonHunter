9abb54cc7f68f61c4cea7b9a49b928e92521e945
David Steele <dsteele2@mail.niagara.edu>
2025-11-29 17:53:06 -0500

    Added corner demons
    Rectangular demons spawn at random corners in 2 second intervals. Motion detected in that corner will cause the demon to disappear and increment the player score variable by 1 point.

--- SteeleCPDH.py
+++ SteeleCPDH.py
@@ -5,15 +5,68 @@ print('This is SteeleCPDH.py')
 
 import numpy as np
 import cv2 as cv
-import os
-import pygame
+import os, pygame, random, time
 
 # get directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 audio_file = os.path.join(script_dir, "golden_parody.mp3")
-first = True
+# game state variables
 score = 0
+corner = 0
+first = True
+tl = tr = bl = br = False
+CORNER_SPAWN_TIME = 2.0     # seconds
+MIDDLE_SPAWN_TIME = 10.0    # seconds
+TOUCH_THRESHOLD = 100000
 FONT = cv.FONT_HERSHEY_SIMPLEX
+# function definitions
+def chooseCorner():
+    global corner
+    corner = random.randint(1,4)
+    match corner:
+        case 1:
+            global tl
+            tl = True
+        case 2:
+            global tr
+            tr = True
+        case 3:
+            global bl
+            bl = True
+        case 4:
+            global br
+            br = True
+
+def spawnDemon():
+    if tl == True:
+        cv.rectangle(frame, (0,0), (80,80), (0,0,0), -1)
+        cv.putText(frame, "D", (20,60),
+            FONT,
+            2.0,            # fontScale
+            (0,0,255),      # color
+            4)              # thickness
+    if tr == True:
+        cv.rectangle(frame, (width-80,0), (width,80), (0,0,0), -1)
+        cv.putText(frame, "D", (width-60,60),
+            FONT,
+            2.0,            # fontScale
+            (0,0,255),      # color
+            4)              # thickness
+    if bl == True:
+        cv.rectangle(frame, (0,height-80), (80,height), (0,0,0), -1)
+        cv.putText(frame, "D", (20,height-20),
+            FONT,
+            2.0,            # fontScale
+            (0,0,255),      # color
+            4)              # thickness
+    if br == True:
+        cv.rectangle(frame, (width-80,height-80), (width,height), (0,0,0), -1)
+        cv.putText(frame, "D", (width-60,height-20),
+            FONT,
+            2.0,            # fontScale
+            (0,0,255),      # color
+            4)              # thickness
+
 # Create a new OpenCV cascade classifier and
 # load the Haar wavelet profile for a face.
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -28,7 +81,16 @@ pygame.mixer.init()
 pygame.mixer.music.load(audio_file)
 pygame.mixer.music.play(-1)
 
+lastCornerSpawn = time.time()
+lastMiddleSpawn = time.time()
+
 while True:
+    now = time.time()
+    if not tl and not tr and not bl and not br:
+        if now - lastCornerSpawn >= CORNER_SPAWN_TIME:
+            chooseCorner()
+            lastCornerSpawn = now
+
     # Capture frame-by-frame
     ret, frame = cap.read()
 
@@ -47,10 +109,28 @@ while True:
         first = False
 
     # Calculate change in gray
-    #delta = cv.absdiff(gray, prev_gray).sum()
-    delta = cv.absdiff(gray[0:100, 0:100], prev_gray[0:100, 0:100]).sum()
-    cv.rectangle(frame, (0,0), (100,100), (100,100,100), 2)
+    delta1 = cv.absdiff(gray[0:80, 0:80], prev_gray[0:80, 0:80]).sum()
+    delta2 = cv.absdiff(gray[0:80, 560:640], prev_gray[0:80, 560:640]).sum()
+    delta3 = cv.absdiff(gray[380:460, 0:80], prev_gray[380:460, 0:80]).sum()
+    delta4 = cv.absdiff(gray[380:460, 560:640], prev_gray[380:460, 560:640]).sum()
+    if tl == True and delta1 >= TOUCH_THRESHOLD:
+        tl = False
+        lastCornerSpawn = now
+        score += 1
+    if tr == True and delta2 >= TOUCH_THRESHOLD:
+        tr = False
+        lastCornerSpawn = now
+        score += 1
+    if bl == True and delta3 >= TOUCH_THRESHOLD:
+        bl = False
+        lastCornerSpawn = now
+        score += 1
+    if br == True and delta4 >= TOUCH_THRESHOLD:
+        br = False
+        lastCornerSpawn = now
+        score += 1
 
+    spawnDemon()
     # Detect faces
     # Uses CascadeClassifier member function to create
     # a list of face objects
@@ -62,11 +142,6 @@ while True:
         cv.rectangle(frame, (x+2, y+2), (x+w-4, y+h-4), (132,38,80), 2)
 
     # Display text
-    cv.putText(frame, str(delta), (50,300),
-        FONT,
-        2.0,            # fontScale
-        (255,255,255),  # color
-        4)              # thickness
     cv.putText(frame, f"Score: {score}", (180, 460),
         FONT,
         2.0,            # fontScale
